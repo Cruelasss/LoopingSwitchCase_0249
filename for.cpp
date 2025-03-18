3aa5e2c6884ebdb8040bce1910bbc8f1d4523b3e
Cruelasss <irula5198@gmail.com>
2025-03-18 10:57:50 +0700

    Membuat perulangan for untuk pencacah naik

--- for.cpp
+++ for.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 
 int main(){
-   
+    int x;
+    //perulangan for untuk pencacah naik 
+    for(x=0; x <= 4;  x++){
+        cout << "Nilai Pencacah =" << x << "Selamat Datang" << endl;
+    }
+    cout << "Nilai Pencacah Terakhir = "<<x;
+    cout << endl;
+
+    for(x= 5; x > 0; x--){
+        cout << "Nilai Pencacah"<< x << "Selamat Datang" << endl;
+    }
+
 }
