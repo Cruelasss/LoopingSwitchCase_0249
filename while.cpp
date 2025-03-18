f177b009cebde9f2e3f3abe512fa4fee4959efef
Cruelasss <irula5198@gmail.com>
2025-03-18 11:25:18 +0700

    Membuat Perulangan For

--- while.cpp
+++ while.cpp
@@ -6,5 +6,20 @@ using namespace std;
 
 int main()
 {
-  
+    int n;
+    int perulanganWhile = 0;
+    int PerulanganDo = 0;
+
+    //perulangan dengan for 
+    cout << "perulangan for pencacah naik" << endl;
+    for (n = 0; n < 5; n++)
+{
+    cout << "nilai n = " << n << "selamat datang" << endl;
+}
+cout << "nilai n terakhir = " << n << endl;
+cout << endl;
+
+cout << "perulangan for pencacah turun" << endl;
+
+
 }
